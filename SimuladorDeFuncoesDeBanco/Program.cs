58f76f3e21d4334401bcfb563d685804aa41098f
Pedro Henrique <pedrohbr226@gmail.com>
2023-03-21 14:47:31 -0300

    Banco

--- SimuladorDeFuncoesDeBanco/Program.cs
+++ SimuladorDeFuncoesDeBanco/Program.cs
@@ -24,14 +24,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.MapGraphQLHttp();
-
 app.Run();
 
